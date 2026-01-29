11364ac82873e06c52a50f71786dfff616f58752
Deepika Agrawal <142203258+deepika-agrawal16@users.noreply.github.com>
2026-01-29 23:49:31 +0530

    Update SignupForm.jsx

--- frontend/src/components/auth/SignupForm.jsx
+++ frontend/src/components/auth/SignupForm.jsx
@@ -51,7 +51,7 @@ const SignupForm = () => {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch("https://chargeev-backend-g7ik.onrender.com/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ...formData, role }),
